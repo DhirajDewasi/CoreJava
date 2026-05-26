4e2876f9d6377c5986de5873912b99fe9124b299
unknown <dewasidhiru32@gmail.com>
2026-05-26 14:11:15 +0530

    First commit

---
+++ TestMatrix.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class TestMatrix{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int r,c;
+		System.out.println("Enter a Row:");
+		r=sc.nextInt();
+		System.out.println("Enter a Column:");
+		c=sc.nextInt();
+		
+		int a[][]=new int[r][c];
+
+		accept(a,r,c,sc);
+		display(a,r,c);
+
+	}
+	public static void accept(int a[][],int r,int c,Scanner sc){
+		System.out.println("Enter Matrix Elements:");
+		for(int i=0;i<r;i++){
+			for(int j=0;j<c;j++){
+				a[i][j]=sc.nextInt();
+			}
+		}
+	
+	}
+	public static void display(int a[][],int r,int c){
+		for(int i=0;i<r;i++){
+			for(int j=0;j<c;j++){
+				System.out.print(a[i][j]+" ");
+			}
+			System.out.println();
+		}
+		
+	}
+}
